cebbb5381717154cf5a0586c9528a16732bceeb7
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-01-23 18:42:45 +0100

    Added a class for the Server

--- server.py
+++ server.py
@@ -1,29 +1,22 @@
-
 import socket
 from threading import Thread
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host = "0.0.0.0"
-port = 3000
-
 print("Server.py")
-serversocket.bind((host,port))
 
-print("Listening on port " + str(port))
+class Server:
+    def __init__(self, host="localhost", port=3000):
+        self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serversocket.bind((host,port))
+        print("Listening on port " + str(port))
 
-def run():    
-    serversocket.listen(5)
-    clientsocket, addr = serversocket.accept()
-    while True:
-        rmsg = clientsocket.recv(4096).decode()
-        print("client: ",rmsg)
-        clientsocket.send(rmsg.encode('ascii'))
-        if rmsg == 'quit':
-            clientsocket.send(b"Server is closing your connection")
-            break
-        
-     
-
-t = Thread (target=run)
-t.start()
+    def run(self):
+        self.serversocket.listen(5)
+        clientsocket, addr = self.serversocket.accept()
+        while True:
+            rmsg = clientsocket.recv(4096).decode()
+            print("client: ",rmsg)
+            if rmsg == 'quit':
+                clientsocket.send(b"Server is closing your connection")
+                break
+            else:
+                clientsocket.send(rmsg.encode('ascii')) 
