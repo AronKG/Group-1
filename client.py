edc6739aa43c6f6f08f132c4bb6a8956bcff8da2
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-01-23 18:41:31 +0100

    Added a class for client.py

--- client.py
+++ client.py
@@ -2,20 +2,18 @@ import sys
 import socket
 from threading import Thread
 print("client.py")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = "127.0.0.1"  #sys.argv[1]
-port = 3000 #sys.argv[2]
-
-s.connect((host,port))
-
-while True: 
-    send_msg = input("Write input value: ")
-    s.send(send_msg.encode('ascii'))
-    msg = s.recv(4096)
-    print("server:",msg)
-    if send_msg == "quit":
-        break
+class Client:
+    def __init__(self, host="localhost", port=3000):
+        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.clientsocket.connect((host,port))
     
-s.close()
-print(msg.decode())
+    def send_message(self, message):
+        self.clientsocket.send(message.encode())
+        self.get_response()
+
+    def get_response(self):
+        print("Server: ", self.clientsocket.recv(4096).decode())
+
+    def close_connection(self):
+        self.send_message("quit")
