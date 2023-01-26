5f93039cfb23595ef36fb4ab749a6a7ccfe8910e
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-01-26 20:17:31 +0100

    Simple websocket implementation for the client to allow connection to the websocket server

--- client.py
+++ client.py
@@ -1,19 +1,21 @@
-import sys
-import socket
-from threading import Thread
-print("client.py")
+import asyncio
+import websockets
 
 class Client:
-    def __init__(self, host="localhost", port=3000):
-        self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientsocket.connect((host,port))
     
-    def send_message(self, message):
-        self.clientsocket.send(message.encode())
-        self.get_response()
+    def __init__(self,host,port=3000):
+        self.host = host;
+        self.port = port;
+        self.uri = f"ws://{self.host}:{self.port}"
+        
+    async def start_chat(self):
+        async with websockets.connect(self.uri) as websocket:
+            while True:
+                message = input('Enter your message: ')
+                await websocket.send(message)
+                print(f'Sent: {message}')
+                response = await websocket.recv()
+                print(f'Received: {response}')
 
-    def get_response(self):
-        print("Server: ", self.clientsocket.recv(4096).decode())
-
-    def close_connection(self):
-        self.send_message("quit")
+client = Client(host="192.168.1.4")
+asyncio.get_event_loop().run_until_complete(client.start_chat())
