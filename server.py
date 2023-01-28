014a111b809053b1489aad8949e3c4f2a346063d
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-01-28 15:40:04 +0100

    A mixup between server&client fixed now

--- server.py
+++ server.py
@@ -1,21 +1,38 @@
 import asyncio
 import websockets
 
-class Client:
-    
-    def __init__(self,host,port=3000):
-        self.host = host;
-        self.port = port;
-        self.uri = f"ws://{self.host}:{self.port}"
-        
-    async def start_chat(self):
-        async with websockets.connect(self.uri) as websocket:
-            while True:
-                message = input('Enter your message: ')
-                await websocket.send(message)
-                print(f'Sent: {message}')
-                response = await websocket.recv()
-                print(f'Received: {response}')
+class Server:
+    clients = set()
 
-client = Client(host="192.168.1.4")
-asyncio.get_event_loop().run_until_complete(client.start_chat())
+    def __init__(self,ip="localhost",port=3000):
+        self.ip = ip;
+        self.port = port;
+        
+    async def register(self,websocket):
+        self.clients.add(websocket)
+        print(f'{websocket} connected')
+
+    async def unregister(self,websocket):
+        self.clients.remove(websocket)
+        print(f'{websocket} disconnected')
+
+    async def broadcast(self, message):
+        if self.clients:
+            await asyncio.wait([client.send(message) for client in self.clients])
+
+    async def chat_handler(self, websocket, path):
+        await self.register(websocket)
+        try:
+            async for message in websocket:
+                await self.broadcast(message)
+        finally:
+            await self.unregister(websocket)
+
+    def start_server(self):
+        print(f"Server running on {self.ip} on port {self.port}")
+        start_server = websockets.serve(self.chat_handler, self.ip, self.port)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
+
+server = Server(ip="192.168.1.4")
+server.start_server()
