8d987db967aed7409e17ad14895b801357598714
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-01-31 14:27:51 +0100

    Added input args for ip and port & input for client

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 import sys
 import socket
 from threading import Thread
+
 print("client.py")
 
 class Client:
-    def __init__(self, host="localhost", port=3000):
+    def __init__(self, host, port):
         self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientsocket.connect((host,port))
-    
+        self.clientsocket.connect((host, port))
+
     def send_message(self, message):
         self.clientsocket.send(message.encode())
         self.get_response()
@@ -17,3 +18,15 @@ class Client:
 
     def close_connection(self):
         self.send_message("quit")
+        self.clientsocket.close()
+
+if __name__ == '__main__':
+    host = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 3000
+    client = Client(host, port)
+    while True:
+        message = input("You: ")
+        client.send_message(message)
+        if message == 'quit':
+            break
+    client.close_connection()
