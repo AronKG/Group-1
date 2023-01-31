0ec8fe7a7022b45cdd6204a332653fa1441c3ed8
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-01-31 14:26:09 +0100

    Added input args for ip and port

--- server.py
+++ server.py
@@ -1,22 +1,30 @@
 import socket
+import sys
 from threading import Thread
 
 print("Server.py")
 
 class Server:
-    def __init__(self, host="localhost", port=3000):
+    def __init__(self, host, port):
         self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.serversocket.bind((host,port))
-        print("Listening on port " + str(port))
+        self.serversocket.bind((host, port))
+        print("Listening on " + host + ":" + str(port))
 
     def run(self):
         self.serversocket.listen(5)
         clientsocket, addr = self.serversocket.accept()
+        print("Accepted connection from " + str(addr))
         while True:
             rmsg = clientsocket.recv(4096).decode()
-            print("client: ",rmsg)
+            print("client: ", rmsg)
             if rmsg == 'quit':
                 clientsocket.send(b"Server is closing your connection")
                 break
             else:
-                clientsocket.send(rmsg.encode('ascii')) 
+                clientsocket.send(rmsg.encode('ascii'))
+
+if __name__ == '__main__':
+    host = sys.argv[1] if len(sys.argv) > 1 else 'localhost'
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 3000
+    server = Server(host, port)
+    server.run()
