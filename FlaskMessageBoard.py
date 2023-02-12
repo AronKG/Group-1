1f742d3850f88a38b0ab12315335bfd4f1b41b23
Johan Sollenius <sojo21ma@student.ju.se>
2023-02-12 14:46:29 +0100

    Connected backend with frontend

--- FlaskMessageBoard.py
+++ FlaskMessageBoard.py
@@ -1,30 +1,44 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import SocketIO, emit
+import time
+import os
+import random
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+socketio = SocketIO(app)
+
+messages = []
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/login")
-def login():
+    #Om man redan är inloggad
+    if "username" in session:
+        return redirect("/chat")
+        
     return render_template("login.html")
 
-"""
-messages = []
+@socketio.on("message") #Om vi får socket emit med message (någon har skickat ett meddelande)
+def handle_message(data):
+    messages.append(data) #Så dem som ansluter senare kan se alla gammla meddelanden
+    emit("new_message", data, broadcast=True) #Meddela att ett nytt meddelande har skickats till alla (broadcast)
+    
+@app.route("/chat", methods=["POST", "GET"])
+def chat():
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        message = request.form["message"]
-        messages.append(message)
-    return render_template("index.html")
+    #print(request.form)
+    if request.method == 'POST':
+    
+        #Om någon nyss loggat in
+        if("username" in request.form):
+            session['username'] = request.form['username']
 
-@app.route("/messages")
-def messages_route():
-    return render_template("messages.html", messages=messages)
-"""
+            username = session['username']
+            socketio.emit("new_connect", f"{username} connected to the chat!", broadcast=True) 
+    
+    return render_template("index.html", username=session['username'],messages=messages)
+    
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
 
