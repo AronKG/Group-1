1681bb0ddec3b14176cac12db6b12a7f575e3220
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-02-21 21:21:15 +0100

    New test file for the merged backend&frontend

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+import tempfile
+from server import app, contains_spam
+
+class FlaskTest(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+
+    def test_home(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_chat(self):
+        with self.app as client:
+            with client.session_transaction() as session:
+                session['username'] = 'testuser'
+            response = self.app.get('/chat')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(b'testuser' in response.data)
+
+    def test_contains_spam(self):
+        self.assertTrue(contains_spam('Fuck you'))
+        self.assertFalse(contains_spam('Hello world!'))
+
+if __name__ == '__main__':
+    unittest.main()
