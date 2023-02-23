3738d41a5838ffa81d9bd25f084d6569dbed6dbf
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-02-23 14:22:07 +0100

    Possible fix permitted port&host

--- server.py
+++ server.py
@@ -57,8 +57,8 @@ def contains_spam(data):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default='0.0.0.0')
-    parser.add_argument("--port", type=int, default=80)
+    parser.add_argument("--host", type=str, default='127.0.0.1')
+    parser.add_argument("--port", type=int, default=5000)
     args = parser.parse_args()
 
     socketio.run(app, debug=True, host=args.host, port=args.port, allow_unsafe_werkzeug=True)
