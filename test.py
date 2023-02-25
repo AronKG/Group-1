6e584caeb72af7764ea26849e13dd72fe71c0061
Mohamad Alkahled <mohammaadkhaliid@gmail.com>
2023-02-25 20:42:34 +0100

    adding unit tests

--- test.py
+++ test.py
@@ -35,7 +35,23 @@ class TestChatApp(unittest.TestCase):
         response = self.client.post('/login', data={'username': 'invalid', 'password': 'password'})
         self.assertEqual(response.status_code, 401)
 
- 
+    #Testing that the server works correctly 
+    def test_server_start(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Chat App', response.data)
+        self.assertIn(b'Username', response.data)
+        self.assertEqual(app.config['SERVER_NAME'], 'localhost:500')
+
+    #Testing that that a user can log in successfully by providing a username,
+    def test_login_success(self):
+        response = self.client.post('/login', data={'username': 'Nasim'})
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('username', session)
+        self.assertEqual(session['username'], 'Nasim')
+
+
+        
     #Testing that a user can't use invalied password
     #def test_invalid_password(self):
         #response = self.client.post('/login', data={'username': 'testuser', 'password': 'invalid'})
