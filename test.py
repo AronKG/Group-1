e4d4208106a077377c54cfbd8a723a4146ab6a9f
AronKG <122611265+AronKG@users.noreply.github.com>
2023-02-25 17:00:30 +0100

    make smalle changes

--- test.py
+++ test.py
@@ -23,12 +23,12 @@ class TestChatApp(unittest.TestCase):
     #this will test that users can't login with an empty username
     def test_empty_username(self):
         response = self.client.post('/login',data={'username': '', 'password': 'password'})
-        self.assertEqual(response.status, 400)
+        self.assertEqual(response.status, 404)
     
     # Testing that user can't login with empty passeword
     def tes_empty_password(self): 
         response = self.client.post('/login', data = {'username': 'testuser', 'passworde': ''})
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 404)
 
 if __name__ == '__main__':
     unittest.main()
